7da07260556cc792a4d0db2f20640e162a36da36
Kai Curry <kai@cape.io>
2016-01-05 23:53:29 -0600

    update after debugging with an uno

--- throttle-button.ino
+++ throttle-button.ino
@@ -1,19 +1,25 @@
 // This is a simple button throttle sketch for a digispark.
 
 // Connect this to the controller green wire.
-// Also PCB LED. 5v max out. PWM 504 Hz. 10k Ohm resistor to 2uf cap. Other side of cap to negative.
-int throttlePin = 1;
-// Connect this to one side of the brake output wire.
-int brakePin = 2;
+// Also PCB LED. 5v max out. PWM ~1000 Hz.
+// Attached to a 10k Ohm resistor then 2uf cap. Other side of cap to negative.
+// digispark pin4 is also used by usb. Use pin 5 or 6 on the Uno.
+int throttlePin = 6;
+
+// Connect this to the yellow wire coming out of the controller.
+// This is pulled up by the controller. pin 2 on digispark. 11 on uno.
+int brakePin = 11;
 // Connect this to the button. The other side of the button should be ground.
 // Has internal 1.5k Ohm to 5v pullup resistor.
 int throttleButtonPin = 3;
 
 // The setup routine runs once when you press reset or the board starts.
 void setup() {
+  Serial.begin(9600);
   pinMode(throttlePin, OUTPUT);
   pinMode(brakePin, INPUT);
   pinMode(throttleButtonPin, INPUT);
+  Serial.println("SETUP");
 }
 
 // The loop routine runs over and over again forever.
@@ -25,16 +31,19 @@ void loop() {
 // THROTTLE
 
 int throttleOutput = 0; // off.
-int throttleRamp = 10; // ms
+int throttleRamp = 20; // ms
 
 // When does the controller start to respond to the throttle input voltage?
 // Remember, analogWrite values go from 0 to 255.
 byte throttleMin = 50; // around 1 volt.
-byte throttleMax = 200; // around 4 volts.
+byte throttleMax = 240; // around 4 volts.
+// How much we add each increment. 1 increment = 0.02v.
+byte throttleIncrement = 2;
 // This stores the millisecond time of last throttle adjustment.
 unsigned long throttleLastUp = 0;
 // On the last throttle adjustment, was the throttle on or off.
 boolean throttleWasOn = false;
+
 
 void throttleLoop() {
   // When the brake is on turn the throttle off. End of story.
@@ -45,45 +54,56 @@ void throttleLoop() {
     if (digitalRead(throttleButtonPin) == LOW) {
       throttleUp();
     }
-    // Throttle pin off. Unless the ramp reached throttleMax turn the throttle off.
-    else if (throttleOutput < throttleMax) {
-      throttleOff();
+    else {
+      throttleWasOn = false;
+      // Throttle pin off. Unless the ramp reached throttleMax turn the throttle off.
+      if (throttleOutput < throttleMax) {
+        throttleOff();
+      }
     }
   }
+  // Delay is for debug.
+//  delay(30);
 }
 
 void throttleUp() {
   // Only increase every so often.
   if (millis() - throttleLastUp >= throttleRamp) {
     // With a new button push reset throttle position to the minimum.
-    if (!throttleWasOn) {
-      throttleOutput = throttleMin; 
-    }
     // On start begin where the controller will actually do something.
-    else if (throttleOutput == 0) {
-      throttleOutput = throttleMin;
+    if (!throttleWasOn || throttleOutput == 0) {
+      throttleOutput = throttleMin; 
+      Serial.println("new throttle");
     }
     // Do not go beyond max.
     if (throttleOutput != throttleMax) {
       // Increment one is an increase of +0.02v.
-      throttleOutput++;
+      throttleOutput = throttleOutput + throttleIncrement;
       analogWrite(throttlePin, throttleOutput);
       // Save that the throttle was on.
       throttleWasOn = true;
+      Serial.println(throttleOutput);
     }
     // Save time we updated throttle position.
     throttleLastUp = millis();
   }
 }
+void throttleUpTest() {
+  if (throttleOutput < throttleMax) {
+    throttleOutput = throttleOutput + throttleIncrement;
+    analogWrite(throttlePin, throttleOutput);
+  }
+}
 
 // Turn the throttle off.
 void throttleOff() {
-  if (throttleWasOn) {
+  if (throttleOutput > 0) {
     // Save throttle output value.
     throttleOutput = 0;
     analogWrite(throttlePin, throttleOutput);
     // Save that the throttle was off.
     throttleWasOn = false;
+    Serial.println("BRAKE");
   }
 }
 
